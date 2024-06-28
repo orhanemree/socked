5c9b7598e5418cacad4bd86028aa1fd3c0d7934d
orhanemree <orhanemre.dev@gmail.com>
2024-06-28 19:12:58 +0300

    fix: fix sc_trim() helper
    it was completely wrong :(

--- src/helpers.c
+++ src/helpers.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "../include/helpers.h"
 
 
 char *sc_trim(char *str) {
 
-    // use free() after
+    // use free after
+
+    if (str == NULL) return NULL;
 
     size_t len = strlen(str);
+    const char *start = str;
+    const char *end = str + len - 1;
 
-    char *trimmed = (char *) malloc((len+1)*sizeof(char));
-    memset(trimmed, '\0', len+1);
-
-    int j = 0;
-
-    for (int i = 0; i < len; ++i) {
-        if (str[i] != ' ' &&
-            str[i] != '\t' &&
-            str[i] != '\n' &&
-            str[i] != '\r' &&
-            str[i] != '\v') {
-
-                trimmed[j] = str[i];
-                j++;
-            }
+    while (isspace(*start)) {
+        start++;
     }
 
-    return trimmed;
+    while (end > start && isspace(*end)) {
+        end--;
+    }
+
+    size_t trimmed_len = (end - start) + 1;
+
+    char *trimmed_str = (char *) malloc((trimmed_len + 1) * sizeof(char));
+
+    // TODO: add error handle
+
+    strncpy(trimmed_str, start, trimmed_len);
+    trimmed_str[trimmed_len] = '\0';
+
+    return trimmed_str;
 }
